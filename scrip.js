85bfda2f58eac2e7f82f445531ea497bc63cc69f
Dylan <108439549+Dylankulik@users.noreply.github.com>
2024-08-31 14:33:06 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -8,12 +8,30 @@ function downloadPDF() {
     }
 
     const reader = new FileReader();
-    reader.onload = function(e) {
+    reader.onload = function (e) {
         const { jsPDF } = window.jspdf;
         const doc = new jsPDF();
 
-        doc.addImage(e.target.result, 'JPEG', 15, 40, 180, 160);
-        doc.save('imagen.pdf');
+        const img = new Image();
+        img.onload = function () {
+            const imgWidth = img.width;
+            const imgHeight = img.height;
+
+            const pageWidth = doc.internal.pageSize.getWidth();
+            const pageHeight = doc.internal.pageSize.getHeight();
+
+            const scale = Math.min(pageWidth / imgWidth, pageHeight / imgHeight);
+
+            const newWidth = imgWidth * scale;
+            const newHeight = imgHeight * scale;
+
+            const xOffset = (pageWidth - newWidth) / 2;
+            const yOffset = (pageHeight - newHeight) / 2;
+
+            doc.addImage(img, 'JPEG', xOffset, yOffset, newWidth, newHeight);
+            doc.save('imagen.pdf');
+        };
+        img.src = e.target.result;
     };
 
     reader.readAsDataURL(file);
